58091b939bdb100e525c2dad2f36dd908ebd5048
Gabriel Ong <gabrielongxe@gmail.com>
2020-04-08 17:42:35 +0800

    updated for observation 1 & 2

--- Sentiment Analysis/check.py
+++ Sentiment Analysis/check.py
@@ -1,20 +1,34 @@
-# sudo code 
+# sudo code part 1
+"""
+tense_songs = []
+ for song in songs: 
+     if song is tense: 
+        # retrieve users emotional state and ID and store in a list 
+        tense_songs.append(song)
+        
 
-#tense_songs = []
-# for each song 
-    # if song is tense: 
-    # tense_songs.append(song)
-    # retrieve users emotional state and ID and store in a list 
-
-#above_low = []
-#low =[]
-# for t_song in tense_songs:    
-    # if users emotional state is above low :
+above_low = []
+low =[]
+ for t_song in tense_songs:    
+     if users emotional state is above low :
         #above_low.append(user id = unique?)
-    # else:
+     else:
         # low.append(userid=unique?)
-# 
+ 
 
-#print user id and emotional state and song state 
-#print % of users with tense song state/ low song state
-# output : 90%
+print user id and emotional state and song state 
+print (% of users with tense song state/ low song state)
+ output : 90%
+"""
+#sudo code part 2 
+""" 
+mental_low = []
+mental high = []
+for song in songs:
+    if song == "upbeat" & user mental:       
+            mental_low.append(unique user_id)
+        else:
+            mental_high.append(unique user id)
+
+print (mental low / mental high x 100% ) # to show that users have good mental health 
+            """
