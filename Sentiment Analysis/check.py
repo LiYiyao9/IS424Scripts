f653eb6ac988d3d0848b22b434c4b1fa2d16c64a
Gabriel Ong <gabrielongxe@gmail.com>
2020-04-08 14:54:27 +0800

    sudo code of what i wanna do

---
+++ Sentiment Analysis/check.py
@@ -0,0 +1,20 @@
+# sudo code 
+
+#tense_songs = []
+# for each song 
+    # if song is tense: 
+    # tense_songs.append(song)
+    # retrieve users emotional state and ID and store in a list 
+
+#above_low = []
+#low =[]
+# for t_song in tense_songs:    
+    # if users emotional state is above low :
+        #above_low.append(user id = unique?)
+    # else:
+        # low.append(userid=unique?)
+# 
+
+#print user id and emotional state and song state 
+#print % of users with tense song state/ low song state
+# output : 90%
